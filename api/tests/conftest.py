fba42e8e6ed1f5156b8fd1b431a5709391f4e2b9
yanchen01 <ychen01@bu.edu>
2022-03-19 23:06:06 -0400

    changed unit test config

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -5,10 +5,6 @@ from app import app as flask_app
 
 @pytest.fixture
 def app():
-    flask_app.config['MONGODB_SETTINGS'] = {
-        'db': 'patient-management-test',
-        'host': 'mongodb://localhost/patient-management-test'
-    }
     yield flask_app
 
 
